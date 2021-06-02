5f58d5924b6b845d018b7d94788811b1b61d6248
vololodin <vololodin@gmail.com>
2021-06-02 11:11:55 +0400

    [upd]

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
-print(os.environ['TOKEN'])
+
 TOKEN = os.environ['TOKEN']
 
 
